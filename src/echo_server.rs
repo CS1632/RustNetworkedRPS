aba76a722118b0669d9c3bdefb557c32cd447be6
NMM103 <NMM103@pitt.edu>
2022-10-22 18:36:46 -0400

    Created an echo server to test functionality

---
+++ src/echo_server.rs
@@ -0,0 +1,30 @@
+use std::io::prelude::*;
+use std::io::{Read, Write, Error};
+use std::thread;
+use std::net::{TcpListener, TcpStream};
+
+
+fn main() {
+	let listener = TcpListener::bind("0.0.0.0:8888")
+		.expect("Could not bind");
+		for stream in listener.incoming() {
+			match stream {
+				Err(e) => { eprintln!("failed: {}", e)}
+				Ok(stream) => {
+					thread::spawn(move || {
+						handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}", error));
+					});
+				}
+			}
+		}
+}
+
+fn handle_client(mut stream: TcpStream) -> Result<(), Error>{
+	println!("Incoming connection from: {}", stream.peer_addr()?);
+	let mut buf = [0;512];
+	loop {
+		let bytes_read = stream.read(&mut buf)?;
+		if bytes_read == 0 {return Ok(())}
+		stream.write(&buf[..bytes_read])?;
+	}
+}
